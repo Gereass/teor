7e66cedb8bcae3856dc479c29d586d182c56eecd
gereass <gereas@vk.com>
2025-05-06 09:40:42 +0300

    add new file

--- chtooto.py
+++ chtooto.py
@@ -1,12 +1,1 @@
-class Person:
-    def __init__(self, name):
-        self.name = name
-
-    def say_hello(self):
-        print(f"Hello, my name is {self.name}")
-        self.a = 47
-        print(self.a)
-        
-
-person = Person("Alice")
-person.say_hello() # выведет "Hello, my name is Alice"
+a=1
